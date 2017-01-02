3977480bd843986f908d56260b2c6e263003d8ec
abhitrip <atripath@eng.ucsd.edu>
2017-01-02 09:38:21 -0800

    Changing changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#main").append("Abhijit");
+ var name = "Abhijit";
+ console.log(name);
+var email = "cameron@udacity.com";
+var newEmail = email.replace("udacity","gmail");
+
+console.log(newEmail);
